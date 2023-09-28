80fe4489dc05a65d58241fe7869e717fcbb34c3e
Dmitry Kralichkin <dm_krl@mail.ru>
2023-09-29 00:07:33 +0700

    ready SideBar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import BarPlayer from './components/BarPlayer';
 import CenterBlockFilter from './components/CenterBlockFilter';
 import SectionMusicList from './components/SectionMusicList';
 import SectionsNav from './components/SectionsNav';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
@@ -27,47 +28,7 @@ function App() {
               <CenterBlockFilter />
               <SectionMusicList />
             </div>
-            <div className="main__sidebar sidebar">
-              <div className="sidebar__personal">
-                <p className="sidebar__personal-name">Sergey.Ivanov</p>
-                <div className="sidebar__icon">
-                  <svg alt="logout">
-                    <use xlinkHref="img/icon/sprite.svg#logout" />
-                  </svg>
-                </div>
-              </div>
-              <div className="sidebar__block">
-                <div className="sidebar__list">
-                  <div className="sidebar__item">
-                    <a className="sidebar__link" href="./">
-                      <img
-                        className="sidebar__img"
-                        src="img/playlist01.png"
-                        alt="day's playlist"
-                      />
-                    </a>
-                  </div>
-                  <div className="sidebar__item">
-                    <a className="sidebar__link" href="./">
-                      <img
-                        className="sidebar__img"
-                        src="img/playlist02.png"
-                        alt="day's playlist"
-                      />
-                    </a>
-                  </div>
-                  <div className="sidebar__item">
-                    <a className="sidebar__link" href="./">
-                      <img
-                        className="sidebar__img"
-                        src="img/playlist03.png"
-                        alt="day's playlist"
-                      />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <SideBar />
           </main>
           <BarPlayer />
           <footer className="footer" />
