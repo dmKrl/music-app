2151dd62cc2838e2e2a7978dba76c8986020668e
Dmitry Kralichkin <dm_krl@mail.ru>
2023-09-29 00:21:31 +0700

    ready SearchInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import BarPlayer from './components/BarPlayer';
 import CenterBlockFilter from './components/CenterBlockFilter';
+import SearchInput from './components/SearchInput';
 import SectionMusicList from './components/SectionMusicList';
 import SectionsNav from './components/SectionsNav';
 import SideBar from './components/SideBar';
@@ -13,18 +14,7 @@ function App() {
           <main className="main">
             <SectionsNav />
             <div className="main__centerblock centerblock">
-              <div className="centerblock__search search">
-                <svg className="search__svg">
-                  <use xlinkHref="img/icon/sprite.svg#icon-search" />
-                </svg>
-                <input
-                  className="search__text"
-                  type="search"
-                  placeholder="Поиск"
-                  name="search"
-                />
-              </div>
-              <h2 className="centerblock__h2">Треки</h2>
+              <SearchInput />
               <CenterBlockFilter />
               <SectionMusicList />
             </div>
--- src/components/CenterBlockFilter.jsx
+++ src/components/CenterBlockFilter.jsx
@@ -3,12 +3,15 @@ import ButtonFilter from './UI/ButtonFilter';
 
 function CenterBlockFilter() {
   return (
-    <div className="centerblock__filter filter">
-      <div className="filter__title">Искать по:</div>
-      <ButtonFilter>Исполнителю</ButtonFilter>
-      <ButtonFilter>Году выпуска</ButtonFilter>
-      <ButtonFilter>Жанру</ButtonFilter>
-    </div>
+    <>
+      <h2 className="centerblock__h2">Треки</h2>
+      <div className="centerblock__filter filter">
+        <div className="filter__title">Искать по:</div>
+        <ButtonFilter>Исполнителю</ButtonFilter>
+        <ButtonFilter>Году выпуска</ButtonFilter>
+        <ButtonFilter>Жанру</ButtonFilter>
+      </div>
+    </>
   );
 }
 export default CenterBlockFilter;
