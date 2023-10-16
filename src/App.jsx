8972308bbfe2e8f567f896e2412be4b996f8f6ac
Dmitry Kralichkin <dm_krl@mail.ru>
2023-10-16 17:42:18 +0700

    add pages components

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,7 @@
-import { useEffect, useState } from 'react';
-import BarPlayer from './components/BarPlayer/BarPlayer';
-import CenterBlockFilter from './components/Main/CenterBlockFilter';
-import SearchInput from './components/Main/SearchInput';
-import SectionMusicList from './components/Main/SectionMusicList';
-import SectionsNav from './components/SectionNav/SectionsNav';
-import SideBar from './components/SideBar/SideBar';
-import * as S from './App.styles';
-import GlobalStyle from './GlobalStyle.styles';
+import MainPage from './pages/MainPage';
 
 function App() {
-  const [visibleNav, setVisibleNav] = useState(true);
-  const [loadingPage, setLoadingPage] = useState(true);
-  const [activeFilter, setActiveFilter] = useState(null);
-
-  const handlerSelectCategory = (string) =>
-    activeFilter === string ? setActiveFilter(null) : setActiveFilter(string);
-
-  const handlerVisibleNav = () => setVisibleNav(!visibleNav);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoadingPage(!loadingPage);
-    }, 5000);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <>
-      <GlobalStyle />
-      <S.Wrapper>
-        <S.Container>
-          <S.Main>
-            <SectionsNav onClick={handlerVisibleNav} visible={visibleNav} />
-            <S.MainCnterBlock>
-              <SearchInput />
-              <CenterBlockFilter
-                onClick={handlerSelectCategory}
-                activeFilter={activeFilter}
-              />
-              <SectionMusicList loadingPage={loadingPage} />
-            </S.MainCnterBlock>
-            <SideBar loadingPage={loadingPage} />
-          </S.Main>
-          <BarPlayer />
-          <footer />
-        </S.Container>
-      </S.Wrapper>
-    </>
-  );
+  return <MainPage />;
 }
 
 export default App;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import BarPlayer from '../components/BarPlayer/BarPlayer';
+import CenterBlockFilter from '../components/Main/CenterBlockFilter';
+import SearchInput from '../components/Main/SearchInput';
+import SectionMusicList from '../components/Main/SectionMusicList';
+import SectionsNav from '../components/SectionNav/SectionsNav';
+import SideBar from '../components/SideBar/SideBar';
+import GlobalStyle from '../GlobalStyle.styles';
+import * as S from '../App.styles';
+
+function MainPages() {
+  const [visibleNav, setVisibleNav] = useState(true);
+  const [loadingPage, setLoadingPage] = useState(true);
+  const [activeFilter, setActiveFilter] = useState(null);
+
+  const handlerSelectCategory = (string) =>
+    activeFilter === string ? setActiveFilter(null) : setActiveFilter(string);
+
+  const handlerVisibleNav = () => setVisibleNav(!visibleNav);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingPage(!loadingPage);
+    }, 5000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <S.Wrapper>
+        <S.Container>
+          <S.Main>
+            <SectionsNav onClick={handlerVisibleNav} visible={visibleNav} />
+            <S.MainCnterBlock>
+              <SearchInput />
+              <CenterBlockFilter
+                onClick={handlerSelectCategory}
+                activeFilter={activeFilter}
+              />
+              <SectionMusicList loadingPage={loadingPage} />
+            </S.MainCnterBlock>
+            <SideBar loadingPage={loadingPage} />
+          </S.Main>
+          <BarPlayer />
+          <footer />
+        </S.Container>
+      </S.Wrapper>
+    </>
+  );
+}
+
+export default MainPages;
